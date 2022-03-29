2d468976389d6ece1be311a1002c5c99d894ddaa
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-03-29 16:49:20 -0700

    user session test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('backend-auth-api routes', () => {
   beforeEach(() => {
@@ -19,4 +20,21 @@ describe('backend-auth-api routes', () => {
 
     expect(res.body).toEqual({ id: expect.any(String), username: 'max' });
   });
+
+  it('start a new user session', async () => {
+    const user = await UserService.create({
+      username: 'max',
+      password: 'ballislife',
+    });
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ username: 'max', password: 'ballislife' });
+
+    expect(res.body).toEqual({
+      message: 'you have been logged in',
+      user,
+    });
+  });
+
+
 });
